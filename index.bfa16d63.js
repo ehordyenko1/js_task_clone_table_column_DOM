27d8ba2c09836f6dbe728cbfb6ecc30c07743c21
Yehor Gordiienko <egordienko319@gmail.com>
2024-08-06 12:49:34 +0300

    make build

---
+++ index.bfa16d63.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("tr").forEach(function(t){var e=t.cells[1].textContent;t.cells[4].textContent=e;var n=t.cells[4].textContent;t.insertCell(5).textContent=n});
+//# sourceMappingURL=index.bfa16d63.js.map
